6ea503e73dde2eac36ca86a01666b35620d76d2f
yunihidayani18 <hidayaniyuni@gmail.com>
2019-11-04 22:40:20 +0700

    first commit

---
+++ latihan2.md
@@ -0,0 +1,10 @@
+a=int(input("masukkan bilangan1 = "))
+b=int(input("masukkan bilangan2 = "))
+c=int(input("masukkan bilangan3 = "))
+
+if a>b and a>c:
+        print (a, "terbesar")
+elif b>a and b>c:
+        print (b, "terbesar")
+else:
+        print (c, "terbesar")
---
+++ latihan2.py
@@ -0,0 +1,21 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: C:/Users/MyBook 11 Plus/Documents/labspy02/latihan2.py ======
+masukkan bilangan1 = 100
+masukkan bilangan2 = 65
+masukkan bilangan3 = 50
+100 terbesar
+>>> 
+====== RESTART: C:/Users/MyBook 11 Plus/Documents/labspy02/latihan2.py ======
+masukkan bilangan1 = 55
+masukkan bilangan2 = 90
+masukkan bilangan3 = 50
+90 terbesar
+>>> 
+====== RESTART: C:/Users/MyBook 11 Plus/Documents/labspy02/latihan2.py ======
+masukkan bilangan1 = 55
+masukkan bilangan2 = 90
+masukkan bilangan3 = 95
+95 terbesar
+>>> 
